f7036aabfcb739b5e2835697f8d09d557be11de8
fortune-man <wnguddl96@knou.ac.kr>
2024-03-07 20:11:26 +0900

    Chore: 단위테스트 / 애플리케이션 테스트 / 컨트롤러 테스트, 환경설정에서 Gradle 위임 단계 생략으로 자바로 바로 실행 통한 빌드 속도 개선, 어노테이션 프로세서 허용

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,17 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+		String hello = "hello";
+
+		Assertions.assertEquals(hello, "hello");
+	}
+
+}
